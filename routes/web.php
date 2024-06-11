f021ce4b43c7bf6c77e9bfa1f524fedc14f41aa1
Santhiago <santhiagosantos7@gmail.com>
2024-06-10 23:05:18 -0300

    Cria controllers: Principal,Contato e Sobre Nós

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return 'Iniciando os estudos sobre o Laravel';
-});
-Route::get('/sobre-nos', function () {
-    return 'Página Sobre nós';
-});
-Route::get('/contato', function () {
-    return 'Página de contato';
-});
+
+Route::get('/', [PrincipalController::class, 'principal']);
+Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos']);
+Route::get('/contato', [ContatoController::class, 'contato']);
+
